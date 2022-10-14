d895d0914f1cf8c2d8ed80874c4488e1367a68cc
GeneralDarian <binnerdr@yahoo.com>
2022-10-14 17:51:13 -0500

    Added eur!info

--- src/main.py
+++ src/main.py
@@ -486,6 +486,47 @@ Here is the location of the euro banknote serials for both Series 1 (top) and Se
             )
             using_tocoin = False
             return
+        
+        
+    @client.command()
+    async def info(ctx):
+        embed = discord.Embed(
+            title="/r/EuroCoins - Server and Bot Information",
+            url="https://github.com/GeneralDarian/EuroBot"
+        )
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/icons/555331630206681108/ce006b24f0d5fb90fbbcfc53c17ab4a2"
+        )
+        embed.add_field(
+            name="Server Members",
+            value=len([user for user in client.users if not user.bot]),
+            inline=True
+        )
+        traders_role = ctx.message.guild.get_role(int(findOfTheWeek.TRADER_ROLE_ID))
+        embed.add_field(
+            name="Verified Traders",
+            value=len(traders_role.members)
+        )
+        guild_creation_time = ctx.message.guild.created_at
+        guild_diff = (datetime.datetime.now(datetime.timezone.utc) - guild_creation_time).days
+        guild_creation_value = f"{guild_diff} days old\nCreated {guild_creation_time.strftime('%d. %B %Y')}"
+        embed.add_field(
+            name="Server is currently...",
+            value=guild_creation_value,
+        )
+        embed.add_field(
+            name="Bot version:",
+            value="BETA",
+        )
+        embed.add_field(
+            name="EuroBot Discord:",
+            value="https://discord.gg/ev53PnSaXV",
+        )
+        embed.set_footer(
+            text="EuroBot is an open source project created by @General Darian ✓ᵛᵉʳᶦᶠᶦᵉᵈ#8498 and @Mango Man#0669. Click on the embed title to visit the bot's github!",
+            icon_url="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        )
+        await ctx.send(embed=embed)
 
     client.run(DISCORD_TOKEN)
 
