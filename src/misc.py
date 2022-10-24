46da43be3f24dfa77a2b2c2ab22404a5343a1a29
Thomas Voss <mail@thomasvoss.com>
2022-10-24 19:15:41 +0200

    Include latency in /ping

--- src/misc.py
+++ src/misc.py
@@ -1,4 +1,4 @@
 def init(client):
     @client.tree.command(name="ping", description="Ping the bot")
     async def ping(interaction):
-        await interaction.response.send_message("Pong!")
+        await interaction.response.send_message(f"Pong! {round(client.latency * 1000)}ms")
