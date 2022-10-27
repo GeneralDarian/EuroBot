887193cc3df1ca577fde36bafe2e27ed03bcc141
Thomas Voss <mail@thomasvoss.com>
2022-10-27 15:22:33 +0200

    Add logging to the misc module

--- src/modules/misc.py
+++ src/modules/misc.py
@@ -1,6 +1,12 @@
+import logging
+
+from bot import EuroBot
+
+logger = logging.getLogger(f"{EuroBot.name}.{__name__}")
+
+
 def init(client):
     @client.tree.command(name="ping", description="Ping the bot")
     async def ping(interaction):
-        await interaction.response.send_message(
-            f"Pong! {round(client.latency * 1000)}ms"
-        )
+        logger.debug("Bot pinged!")
+        await client.send(interaction, f"Pong! {round(client.latency * 1000)}ms")
