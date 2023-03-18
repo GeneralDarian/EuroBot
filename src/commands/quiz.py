8722c9b6357f36fb7a91ce18f14a1d279142eaad
generaldarian <binnerdr@yahoo.com>
2023-03-18 20:00:41 +0100

    significant improvements and upgrades to quiz.py

--- src/commands/quiz.py
+++ src/commands/quiz.py
@@ -9,6 +9,7 @@ import discord
 from discord import bot
 from discord.commands import option
 from discord.ext import commands
+from tools import quizAssistant
 
 
 def fetch_answer(db, id):
@@ -25,14 +26,252 @@ def fetch_answer(db, id):
         cur = conn.cursor()
     """db = database
     id = id of question"""
-    db = "design_trivia"
+    print(f"SELECT correct_ans FROM {db} WHERE question_id = {id};")
     cur.execute(f"SELECT correct_ans FROM {db} WHERE question_id = {id};")
     rows = cur.fetchall()[0]
     conn.close()
     return rows[0]
 
+def fetch_question(db):
+    """Fetch a random question from database db"""
+    conn = None
+    db_file = "data/Quiz.db"
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    finally:
+        cur = conn.cursor()
+    """db = database
+    id = id of question"""
+    cur.execute(f"SELECT * FROM {db} ORDER BY RANDOM() LIMIT 1;")
+    rows = cur.fetchall()[0]
+    conn.close()
+    return rows
+
+async def ask_question(data: dict, channel: discord.TextChannel):
+    """Asks a question in channel.
+    channel MUST be a discord.TextChannel option
+    data must be structured as follows:
+    data['quiz_class']: The quiz class
+    data['database']: Name of database
+    data['id']: Question ID
+    data['question']: The question title
+    data['correct_answer']: The question's correct answer
+    data[0]: Option one of the question
+    data[1]: Option two of the question
+    data[2]: Option three of the question
+    data[3]: Option four of the question
+    data[4]: Option five of the question
+    data['mintage']: bool. True if this is a mintage-question (changes how answers are shuffled)
+    data['image']: The image to display with the question
+    data['label_option']: TRUE if you want buttons to be labelled with data[n], FALSE if you want them labeled 'OPTION N'"""
+
+    #determine answer list and get position of correct answer
+    ans_list = [] #this will become the list of answers in order (i.e. ans_list[0] = option 1, etc.)
+
+
+
+    random.shuffle(ans_list)
+
+    if data['mintage'] is True and str(data['correct_answer']).lower() == '5m or higher':
+        correct_ans_index = (-1, 0)
+    elif data['mintage'] is True and str(data['correct_answer']).lower() == 'nifc':
+        correct_ans_index = (-1, 5)
+    elif data['mintage'] is True:
+        leeway = 20000
+        if abs(data['correct_answer'] - 5000000) < leeway:
+            correct_ans_index = (0,1)
+        elif data['correct_answer'] > 5000000:
+            correct_ans_index = (-1, 0)
+        elif abs(data['correct_answer'] - 2000000) < leeway:
+            correct_ans_index = (1, 2)
+        elif data['correct_answer'] > 2000000:
+            correct_ans_index = (-1, 1)
+        elif abs(data['correct_answer'] - 1000000) < leeway:
+            correct_ans_index = (2, 3)
+        elif data['correct_answer'] > 1000000:
+            correct_ans_index = (-1, 2)
+        elif abs(data['correct_answer'] - 500000) < leeway:
+            correct_ans_index = (3, 4)
+        elif data['correct_answer'] > 500000:
+            correct_ans_index = (-1, 3)
+        else:
+            correct_ans_index = (-1, 4)
+
+    else:
+        for n in range(4):
+            try:
+                if data[n] is not None:
+                    ans_list.append(data[n])
+                else:
+                    break
+            except KeyError:
+                break
+        random.shuffle(ans_list)
+        correct_ans_index = (random.randint(0, len(ans_list) - 1))
+        temp = ans_list[correct_ans_index]
+        ans_list[correct_ans_index] = data['correct_answer']
+        ans_list.append(temp)
+        correct_ans_Index = (-1, correct_ans_index)
+
+    if data['mintage'] is True:
+        ans_list.append("5 mil or higher")  # index 0
+        ans_list.append("2 mil to 5 mil")  # index 1
+        ans_list.append("1 mil to 2 mil")  # index 2
+        ans_list.append("500k to 1 mil")  # index 3
+        ans_list.append("Below 500k")  # index 4
+        ans_list.append("NIFC")  # index 5
+
+    #make embed and views (buttons)
+    view = discord.ui.View(timeout=20, disable_on_timeout=True)
+    embed = discord.Embed(
+        title=data['question'],
+        color=0xFFCC00,
+    )
+    embed.set_image(url=data['image'])
+
+    for ind, val in enumerate(ans_list):
+        if data['mintage'] is False:
+            embed.add_field(name=f"OPTION {ind+1}", value=val, inline=False)
+
+        #determine label
+        if data['label_option'] is True:
+            label = val
+        else:
+            label = f"OPTION {ind+1}"
+
+        #add buttons
+        if ind in correct_ans_index:
+            print(f"Correct answer is: {label}")
+            view.add_item(
+                QuizButtons(option=label, correct=True, question_id=data['id'], database=data['database'], quiz=data['quiz_class'])
+            )
+        else:
+            view.add_item(
+                QuizButtons(option=label, correct=False, question_id=data['id'], database=data['database'], quiz=data['quiz_class'])
+            )
+
+    #send that shit
+    await channel.send(embed=embed,view=view)
+
+async def mintage_question(channel: discord.TextChannel, quiz: quizAssistant.QuizType):
+    """Sorts out the dictionary which gets sent to ask_question when it is a MINTAGE-BASED question.
+    channel: the channel to send to
+    database: the database which the bot uses to ask for the correct answer to the question."""
+    #0 = id, 1 = country (unused), 2 = deno, 3 = year, 4 = mintage, 5 = yearend, 6 = image
+    #structure: What is the mintage of {database.capitalize()} {deno} {year}?
+
+    #get database
+    database = random.choice(quiz.database_name)
+
+    rows = fetch_question(database)
+    data = {}
+
+    #get year
+    if rows[5] is not None:
+        year = random.randint(rows[3], rows[5])
+    else:
+        year = rows[3]
+
+    #get deno
+    if rows[2] == "1":
+        deno = "1 euro"
+    elif rows[2] == "2":
+        deno = "2 euro"
+    else:
+        deno = rows[2]
+
+    if rows[4].lower() == "5m":
+        data['correct_answer'] = "5M or higher"
+    elif rows[4].lower() == "nifc":
+        data['correct_answer'] = "NIFC"
+    else:
+        data['correct_answer'] = int(rows[4])
+
+    data['database'] = database
+    data['question'] = f"What is the mintage of {database.capitalize()} {deno} {year}?"
+    data['mintage'] = True
+    data['image'] = rows[6]
+    data['id'] = rows[0]
+    data['label_option'] = True
+    data['quiz_class'] = quiz
+
+
+
+    print(data)
+    await ask_question(data, channel)
+    return
+
+async def theory_question(channel: discord.TextChannel, quiz: quizAssistant.QuizType):
+    """Sorts out the dictionary which gets sent to ask_question when it is a THEORY-BASED question.
+    channel: the channel to send to
+    database: the database which the bot uses to ask for the correct answer to the question."""
+    # get database
+    database = random.choice(quiz.database_name)
+
+    rows = fetch_question(database)
+    data = {}
+    data['database'] = 'design_trivia'
+    data['question'] = f"[DT#{rows[0]}] {rows[1]}"
+    data['correct_answer'] = rows[2]
+    data['quiz_class'] = quiz
+
+    for n in range(4):
+        if rows[n + 3] is not None and rows[n + 3] != '':
+            data[n] = rows[n + 3]
+        else:
+            data[n] = None
+
+    data['mintage'] = False
+    data['image'] = rows[7]
+    data['label_option'] = False
+    data['id'] = rows[0]
+
+    await ask_question(data=data, channel=channel)
+    return
+
+async def quiz_handler(channel: discord.TextChannel, quiz: quizAssistant.QuizType):
+    if quiz.mintage is False:
+        await theory_question(channel, quiz)
+    else:
+        await mintage_question(channel, quiz)
+
+
+class NextContinueButton(discord.ui.Button):
+    """Button which shows when you get a question right/wrong and continues the quiz"""
+    def __init__(self, quiz: quizAssistant.QuizType, theory: bool):
+        """Database: database to pull the question from
+        theory: is this a theory based question? (true if yes)"""
+        super().__init__(
+            label="New question!",
+            style=discord.ButtonStyle.success
+        )
+        self.quiz = quiz
+        self.theory = theory
+
+    async def callback(self, interaction: discord.Interaction):
+        if self.theory is True:
+            await theory_question(interaction.channel, self.quiz)
+            await interaction.message.edit(view=None)
+        else:
+            await mintage_question(interaction.channel, self.quiz)
+            await interaction.message.edit(view=None)
+
+class NextCancelButton(discord.ui.Button):
+    """Button which shows when you get a question right/wrong and cancels the quiz"""
+    def __init__(self):
+        super().__init__(
+            label="Cancel Quiz",
+            style=discord.ButtonStyle.danger
+        )
+
+    async def callback(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Quiz has been cancelled.")
+        await interaction.message.edit(view=None)
+
 class QuizButtons(discord.ui.Button):
-    def __init__(self, option, correct, question_id, database):
+    def __init__(self, option, correct, question_id,  quiz: quizAssistant.QuizType, database: str):
         """If the answer is correct, then correct should be True.
         If the answer is not correct, then correct should correspond to the question ID.
         Database must be the name of the database which the question was taken from."""
@@ -41,23 +280,40 @@ class QuizButtons(discord.ui.Button):
             style=discord.ButtonStyle.primary
         )
         self.correct = correct
+        self.quiz = quiz
         self.database = database
         self.question_id = question_id
 
     async def callback(self, interaction: discord.Interaction):
+        desc_embed = fetch_answer(db=self.database, id=self.question_id)
+        if self.quiz.mintage is True: #report the mintage if the quiz is mintage-based
+            if desc_embed.lower() == "nifc":
+                desc_embed = "The coin is NIFC"
+            elif desc_embed.lower() == "5m":
+                desc_embed = "Mintage: 5M or higher"
+            else:
+                desc_embed = f"Mintage: {int(desc_embed):,}"
+
         if self.correct is True:
             embed = discord.Embed(
                 title=f"Correct Answer! [#{self.question_id}]",
-                description=fetch_answer(db=self.database, id=self.question_id),
+                description=desc_embed,
                 color=0x00CC00
             )
         else:
             embed = discord.Embed(
                 title = f"Incorrect! [#{self.question_id}]",
-                description=f"The correct answer is: {fetch_answer(db=self.database, id=self.question_id)}",
+                description=f"The correct answer is: {desc_embed}",
                 color=0xFF0000
             )
-        await interaction.response.send_message(embed=embed)
+        view = discord.ui.View(timeout=20, disable_on_timeout=True)
+        view.add_item(
+            NextContinueButton(theory=False, quiz=self.quiz)
+        )
+        view.add_item(
+            NextCancelButton()
+        )
+        await interaction.response.send_message(embed=embed,view=view)
 
         await interaction.message.edit(view=None)
 
@@ -73,86 +329,43 @@ class Quiz(commands.Cog):
         logging.info(
             f"Cog {path.basename(__file__).removesuffix('.py')} loaded successfully"
         )
-        self.create_connection(r"data\Quiz.db")
-
-    def create_connection(self, db_file):
-        """ create a database connection to a SQLite database """
-        conn = None
-        try:
-            conn = sqlite3.connect(db_file)
-            print(sqlite3.version)
-        except Error as e:
-            print(e)
-        finally:
-            print("Success at connection!")
-            self.cur = conn.cursor()
-
-    def fetch_answer(self, db, id):
-        """db = database
-        id = id of question"""
-        db = "design_trivia"
-        self.cur.execute(f"SELECT correct_ans FROM {db} WHERE question = {id};")
-        rows = self.cur.fetchall()[0]
-        return rows[0]
 
 
     @bot.command(description="Start a quiz!")
-    async def askquestion(self, ctx):
-        self.cur.execute("SELECT * FROM design_trivia ORDER BY RANDOM() LIMIT 1;")
-        rows = self.cur.fetchall()[0]
-
-
-        id = rows[0]
-        questdesc = rows[1]
-        correct_ans = rows[2]
-        ot_ans_1 = rows[3]
-        ot_ans_2 = rows[4]
-        ot_ans_3 = rows[5]
-        ot_ans_4 = rows[6]
-        img = rows[7]
-
-        ans_list = [ot_ans_1]
-        if ot_ans_2 != None:
-            ans_list.append(ot_ans_2)
-        if ot_ans_3 != None:
-            ans_list.append(ot_ans_3)
-        if ot_ans_4 != None:
-            ans_list.append(ot_ans_4)
-
-        random.shuffle(ans_list)
-        correct_ans_index = random.randint(0, len(ans_list)-1)
-        move_ans = ans_list[correct_ans_index]
-        ans_list[correct_ans_index] = correct_ans
-        ans_list.append(move_ans)
-
-        view = discord.ui.View(timeout=20, disable_on_timeout=True)
-
-        embed = discord.Embed(
-            title=f"[#DT{id}] {questdesc}",
-            color=0xFFCC00,
-        )
-        embed.set_image(url=img)
-        ans = 1
-        for ansloop in ans_list:
-            embed.add_field(name=f"OPTION {ans}", value=ansloop, inline=False)
-
-            if ans-1 == correct_ans_index:
-                view.add_item(
-                    QuizButtons(option=f"OPTION {ans}", correct=True, question_id=id, database="design_trivia")
-                )
+    @option(
+        "type",
+        description="The type of quiz to start. To get a list run this command without this argument.",
+        required=False,
+    )
+    async def quiz(self, ctx, type):
+        if type is None:
+            desc_theory = "__**Theory-based quizzes**__"
+            desc_mintage = "__**Mintage-based quizzes**__"
+            for quiz_class in quizAssistant.QuizList:
+                if quiz_class.mintage is False:
+                    desc_theory += f"\n**{quiz_class.name}**\n{quiz_class.desc}"
+                else:
+                    desc_mintage += f"\n**{quiz_class.name}**\n{quiz_class.desc}"
+            desc_theory += f"\n\n{desc_mintage}"
+            embed=discord.Embed(
+                title="EuroBot Quizzes",
+                description=desc_theory,
+                color=0xFFCC00
+            )
+            await ctx.respond(embed=embed)
+        else:
+            for quiz_class in quizAssistant.QuizList:
+                if quiz_class.name == type.title():
+                    quiz_type = quiz_class
+                    break
             else:
-                view.add_item(
-                    QuizButtons(option=f"OPTION {ans}", correct=False, question_id=id, database="design_trivia")
-                )
+                await ctx.respond("Error: The quiz type does not exist! Type ``/quiz`` for a list of quizzes.")
+                return
 
-            ans += 1
-
-        await ctx.respond(embed=embed,view=view)
-
-
-
-
-
+            await ctx.respond("Starting quiz...!")
+            await quiz_handler(channel=ctx.interaction.channel, quiz=quiz_type)
+            return
+            #have fun broski
 
 
 
---
+++ src/tools/quizAssistant.py
@@ -0,0 +1,104 @@
+import random
+from math import ceil
+
+class QuizType:
+    def __init__(self, name: str, mintage: bool, desc: str, database_name: list[str]):
+        """name: name of the quiz to be displayed
+        mintage: is this a mintage-based quiz (True) or theory-based quiz (False)?
+        desc: description of the quiz
+        database_name: The name(s) of the database to pull the questions from (in Quiz.db)"""
+        self.name = name
+        self.mintage = mintage
+        self.desc = desc
+        self.database_name = database_name
+
+AN_Table = QuizType('Andorra', True, 'Quiz yourself on the mintages of Andorra!', ['andorra'])
+AT_Table = QuizType('Austria', True, 'Quiz yourself on the mintages of Austria!', ['austria'])
+BE_Table = QuizType('Belgium', True, 'Quiz yourself on the mintages of Belgium!', ['belgium'])
+HR_Table = QuizType('Croatia', True, 'Quiz yourself on the mintages of Croatia!', ['croatia'])
+CY_Table = QuizType('Cyprus', True, 'Quiz yourself on the mintages of Cyprus!', ['cyprus'])
+EU_Table = QuizType('All Coins', True, 'Quiz yourself on the mintages of all EU countries!', ['andorra', 'austria', 'belgium', 'croatia', 'cyprus'])
+
+Design_Table = QuizType('Design', False, 'Quiz yourself on the individual euro coin designs!', ['design_table'])
+
+QuizList = [
+    AN_Table,
+    AT_Table,
+    BE_Table,
+    HR_Table,
+    CY_Table,
+    EU_Table,
+    Design_Table
+]
+
+
+
+
+
+def generateoptions(mintage: str) -> dict:
+    """Generates a list of viable mintages"""
+    max = "5000000"  # max should be 5 mil but if you want to change that for some reason here you go
+    correct_ans = mintage
+
+    # if mintage is 5M/NIFC:
+    # - correct ans should be 5M/NIFC (so that the program knows how to assemble the return)
+    # - mintage should STILL be generated in order to get 4 options
+    # - program should know to generate one more option (hence options_to_generate = 4)
+    if mintage.lower() == "5m or higher":
+        mintage = str(random.randint(10, 2500)) + "000"
+        options_to_generate = 4
+    elif mintage.lower() == "nifc":
+        mintage = str(random.randint(10, 2500)) + "000"
+        options_to_generate = 4
+    else:
+        options_to_generate = 3
+
+    # process for generating random answers
+    # pointer: how many numbers to generate
+    # zeroes: how many zeroes to stick afterwards
+    for ind, val in enumerate(mintage[::-1]):
+        if val != "0":
+            pointer = len(mintage) - ind
+            zeroes = ind
+            break
+
+    if pointer < 2:  # pointer should be at least 2 digits long so that if, say,
+        # "1,000,000" is submitted then itll be reported as "10" and not "1"
+        # (which would lead to complications due to buffer)
+        pointer = 2
+
+    # max[:0] = ''
+    # to prevent int('') from throwing an error
+    # it makes sure zeroes != 0, and if it is, then use int(max)
+    if zeroes != 0:
+        max_pointer = int(max[:-1 * zeroes])
+    else:
+        max_pointer = int(max)
+
+    buffer = int(str(max_pointer)[:-1])  # how spaced apart should the options be? this is 250,000
+    opt_list = []
+
+    # generate all options
+    while True:
+        rand = random.randint(0, max_pointer)
+
+        for num in opt_list:
+            if abs(num - rand) < buffer:
+                break
+        else:
+            opt_list.append(rand)
+
+        if len(opt_list) >= options_to_generate:
+            break
+
+    for ind, val in enumerate(opt_list):
+        # lol this looks stupid
+        drop = random.random()
+        if drop > 0.95 and zeroes >= 2:
+            opt_list[ind] = f"{int(str(val) + (zeroes - 2) * '0'):,}"
+        elif drop > 0.85 and zeroes >= 1:
+            opt_list[ind] = f"{int(str(val) + (zeroes - 1) * '0'):,}"
+        else:
+            opt_list[ind] = f"{int(str(val) + zeroes * '0'):,}"
+
+    return opt_list
